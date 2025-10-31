4905e2b3cfe2bbeabfe19f127189b4e3365b736f
czechbylukas <ucitellukas@gmail.com>
2025-10-31 13:37:47 +0100

    add druhý slovíčka a test

---
+++ data/2_pasttense.js
@@ -0,0 +1,21 @@
+// data/2_actions.js
+const vocab = [
+  { czech: "dělat", english: "to do" },
+  { czech: "plavat", english: "to swim" },
+  { czech: "pracovat", english: "to work" },
+  { czech: "koukat se, dívat se", english: "to watch" },
+  { czech: "spát", english: "to sleep" },
+  { czech: "odpočívat", english: "to rest" },
+  { czech: "sportovat", english: "to do sports" },
+  { czech: "studovat, učit se", english: "to study, to learn" },
+  { czech: "vařit", english: "to cook" },
+  { czech: "telefonovat", english: "to call" },
+  { czech: "Co jsi dělal/a včera?", english: "What were you doing yesterday? (What did you do)" },
+  { czech: "O víkendu jsem pracovala.", english: "I was working at the weekend." },
+  { czech: "Včera večer jsem se dívala na televizi.", english: "Yesterday evening I was watching TV." },
+  { czech: "V noci jsem spala.", english: "At night I was sleeping." },
+  { czech: "V pondělí jsem odpočívala.", english: "On Monday, I was resting." },
+  { czech: "Ve středu jsem studovala / se učila češtinu.", english: "On Wednesday, I was studying / learning Czech (language)." },
+  { czech: "V pátek jsem vařila kuře.", english: "On Friday, I was cooking chicken." },
+  { czech: "V sobotu jsem telefonovala rodině.", english: "On Saturday, I was calling my family." }
+];
