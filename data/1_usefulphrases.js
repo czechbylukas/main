c07175d348c4e1af31243ecb02683c6109d9fd01
czechbylukas <ucitellukas@gmail.com>
2025-11-01 23:14:02 +0100

    Update 1_usefulphrases.js

--- data/1_usefulphrases.js
+++ data/1_usefulphrases.js
@@ -1,32 +1,206 @@
 // data/1_usefulphrases.js
 const vocab = [
-  { czech: "Dobrý den", english: "How do you do (Good day)", pronunciation: "dobree den" },
-  { czech: "Dobré ráno", english: "Good morning", pronunciation: "dobreh rahno" },
-  { czech: "Dobrý večer", english: "Good evening", pronunciation: "dobree vecher" },
-  { czech: "Dobrou noc", english: "Good night", pronunciation: "dobrow nots" },
-  { czech: "Na shledanou", english: "Goodbye", pronunciation: "nascledanow" },
-  { czech: "Prosím", english: "Please", pronunciation: "proseem" },
-  { czech: "Děkuji", english: "Thank you", pronunciation: "dyekuyi" },
-  { czech: "S dovolením", english: "Excuse me", pronunciation: "s dovolenyeem" },
-  { czech: "Lituji", english: "I’m sorry (I regret)", pronunciation: "lituyi" },
-  { czech: "Promiňte", english: "Excuse me, pardon", pronunciation: "promintey" },
-  { czech: "Ano", english: "Yes", pronunciation: "ano" },
-  { czech: "Ne", english: "No", pronunciation: "neh" },
-  { czech: "Mluvíte anglicky?", english: "Do you speak English?", pronunciation: "mluveete anglitsky?" },
-  { czech: "Nerozumím", english: "I don't understand", pronunciation: "nerozumeem" },
-  { czech: "Prosím napište to", english: "Please, write it down", pronunciation: "proseem napishte to" },
-  { czech: "Kde je restaurace", english: "Where is a restaurant", pronunciation: "kde ye restauratse" },
-  { czech: "banka", english: "bank", pronunciation: "banca" },
-  { czech: "Toaleta, Záchod", english: "Bathroom, toilet", pronunciation: "toaleta, zahkod" },
-  { czech: "Muži, Páni", english: "Men", pronunciation: "mushi, pahnyi" },
-  { czech: "Ženy, Dámy", english: "Women", pronunciation: "zheny, dahmy" },
-  { czech: "Kolik to stojí", english: "How much does it cost?", pronunciation: "kolik to stoyee" },
-  { czech: "Pane vrchní!", english: "Waiter!", pronunciation: "pane vrkhnyee" },
-  { czech: "Jedno pivo, prosím", english: "One beer, please", pronunciation: "yedno pivo proseem" },
-  { czech: "Účet, prosím", english: "The check, please!", pronunciation: "platyit proseem" },
-  { czech: "zde, tady (tu)", english: "here", pronunciation: "tady" },
-  { czech: "Co si dáte?", english: "What would you like?", pronunciation: "co si daate" },
-  { czech: "sto korun", english: "hundred crowns", pronunciation: "sto corun" },
-  { czech: "Ještě něco?", english: "Anything else?", pronunciation: "yeshtee nietso" },
-  { czech: "tam", english: "there", pronunciation: "tam" }
+  {
+    czech:       "Dobrý den",
+    english:     "How do you do (Good day)",
+    spanish:     "Buenos días",
+    german:      "Guten Tag",
+    pronunciation: "dobree den"
+  },
+  {
+    czech:       "Dobré ráno",
+    english:     "Good morning",
+    spanish:     "Buenas mañanas",
+    german:      "Guten Morgen",
+    pronunciation: "dobreh rahno"
+  },
+  {
+    czech:       "Dobrý večer",
+    english:     "Good evening",
+    spanish:     "Buenas tardes",
+    german:      "Guten Abend",
+    pronunciation: "dobree vecher"
+  },
+  {
+    czech:       "Dobrou noc",
+    english:     "Good night",
+    spanish:     "Buenas noches",
+    german:      "Gute Nacht",
+    pronunciation: "dobrow nots"
+  },
+  {
+    czech:       "Na shledanou",
+    english:     "Goodbye",
+    spanish:     "Hasta luego",
+    german:      "Auf Wiedersehen",
+    pronunciation: "nascledanow"
+  },
+  {
+    czech:       "Prosím",
+    english:     "Please",
+    spanish:     "Por favor",
+    german:      "Bitte",
+    pronunciation: "proseem"
+  },
+  {
+    czech:       "Děkuji",
+    english:     "Thank you",
+    spanish:     "Gracias",
+    german:      "Danke",
+    pronunciation: "dyekuyi"
+  },
+  {
+    czech:       "S dovolením",
+    english:     "Excuse me",
+    spanish:     "Con permiso",
+    german:      "Entschuldigung (Mit Erlaubnis)",
+    pronunciation: "s dovolenyeem"
+  },
+  {
+    czech:       "Lituji",
+    english:     "I’m sorry (I regret)",
+    spanish:     "Lo siento",
+    german:      "Entschuldigung (Ich bereue es)",
+    pronunciation: "lituyi"
+  },
+  {
+    czech:       "Promiňte",
+    english:     "Excuse me, pardon",
+    spanish:     "Perdón, con permiso",
+    german:      "Entschuldigung (Verzeihung)",
+    pronunciation: "promintey"
+  },
+  {
+    czech:       "Ano",
+    english:     "Yes",
+    spanish:     "Sí",
+    german:      "Ja",
+    pronunciation: "ano"
+  },
+  {
+    czech:       "Ne",
+    english:     "No",
+    spanish:     "No",
+    german:      "Nein",
+    pronunciation: "neh"
+  },
+  {
+    czech:       "Mluvíte anglicky?",
+    english:     "Do you speak English?",
+    spanish:     "¿Habla usted español?",
+    german:      "Sprechen Sie Deutsch?",
+    pronunciation: "mluveete anglitsky?"
+  },
+  {
+    czech:       "Nerozumím",
+    english:     "I don't understand",
+    spanish:     "No entiendo",
+    german:      "Ich verstehe nicht",
+    pronunciation: "nerozumeem"
+  },
+  {
+    czech:       "Prosím napište to",
+    english:     "Please, write it down",
+    spanish:     "Por favor, escríbalo",
+    german:      "Bitte, schreiben Sie es",
+    pronunciation: "proseem napishte to"
+  },
+  {
+    czech:       "Kde je restaurace",
+    english:     "Where is a restaurant",
+    spanish:     "¿Dónde está el restaurante?",
+    german:      "Wo ist ein Restaurant?",
+    pronunciation: "kde ye restauratse"
+  },
+  {
+    czech:       "Směnárna",
+    english:     "Exchange office",
+    spanish:     "Cambio de moneda",
+    german:      "Wechselstube",
+    pronunciation: "smnyenahrna"
+  },
+  {
+    czech:       "Toaleta, Záchod",
+    english:     "Bathroom, toilet",
+    spanish:     "Baños",
+    german:      "Toilette",
+    pronunciation: "toaleta, zahkod"
+  },
+  {
+    czech:       "Muži, Páni",
+    english:     "Men",
+    spanish:     "Hombres",
+    german:      "Männer, Herren",
+    pronunciation: "mushi, pahnyi"
+  },
+  {
+    czech:       "Ženy, Dámy",
+    english:     "Women",
+    spanish:     "Mujeres",
+    german:      "Frauen, Damen",
+    pronunciation: "zheny, dahmy"
+  },
+  {
+    czech:       "Kolik to stojí",
+    english:     "How much does it cost?",
+    spanish:     "¿Cuánto cuesta esto?",
+    german:      "Wie viel kostet es?",
+    pronunciation: "kolik to stoyee"
+  },
+  {
+    czech:       "Pane vrchní!",
+    english:     "Waiter!",
+    spanish:     "¡Camarero!",
+    german:      "Kellner!",
+    pronunciation: "pane vrkhnyee"
+  },
+  {
+    czech:       "Jedno pivo, prosím",
+    english:     "One beer, please",
+    spanish:     "Una cerveza, por favor",
+    german:      "Ein Bier, bitte",
+    pronunciation: "yedno pivo proseem"
+  },
+  {
+    czech:       "Účet, prosím",
+    english:     "The check, please!",
+    spanish:     "La cuenta, por favor",
+    german:      "Rechnung, bitte",
+    pronunciation: "platyit proseem"
+  },
+  {
+    czech:       "zde, tady (tu)",
+    english:     "here",
+    spanish:     "aquí",
+    german:      "hier",
+    pronunciation: "tady"
+  },
+  {
+    czech:       "Co si dáte?",
+    english:     "What would you like?",
+    spanish:     "¿Qué quiere usted? (en restaurante)",
+    german:      "Was möchten Sie?",
+    pronunciation: "co si daate"
+  },
+  {
+    czech:       "sto korun",
+    english:     "hundred crowns",
+    spanish:     "cien coronas",
+    german:      "einhundert Kronen",
+    pronunciation: "sto corun"
+  },
+  {
+    czech:       "Ještě něco?",
+    english:     "Anything else?",
+    spanish:     "¿Algo más?",
+    german:      "Noch etwas?",
+    pronunciation: "yeshtee nietso"
+  },
+  {
+    czech:       "tam",
+    english:     "there",
+    spanish:     "allí / allá",
+    german:      "dort",
+    pronunciation: "tam"
+  }
 ];
