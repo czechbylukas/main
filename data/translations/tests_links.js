17d80a6f1dc4c691654eb6165f657948522dc38b
czechbylukas <ucitellukas@gmail.com>
2025-11-02 01:50:04 +0100

    add test in language versions

---
+++ data/translations/tests_links.js
@@ -0,0 +1,15 @@
+const testLinks = {
+  lesson1: {
+    en: "https://docs.google.com/forms/d/e/1FAIpQLSewHnJaFJZZT2loQHe-JY3g5Y2tCbYecSM8u3TmY6IVEpPOqg/viewform?usp=header",
+    cz: "https://docs.google.com/forms/d/e/1FAIpQLSewHnJaFJZZT2loQHe-JY3g5Y2tCbYecSM8u3TmY6IVEpPOqg/viewform?usp=header",
+    es: "https://docs.google.com/forms/d/e/1FAIpQLSeLwvig7lLI9qebYTr86d4UWhX4w2NOJC0UEWRONH6LZeLXUA/viewform?usp=header",
+    de: "https://docs.google.com/forms/d/e/1FAIpQLSc-ZrNV1mSxv13fdT_MJ4Ejk6rFNkqieWSvFUPeIF7ogG4Mcw/viewform?usp=header"
+  },
+  lesson2: {
+    en: "https://docs.google.com/forms/d/e/1FAIpQLSdnlKBKukZhaCntXIsbwAvk0Ot6E7DhjZIJVB4GxcxAo6_m2Q/viewform?usp=header",
+    cz: "https://docs.google.com/forms/d/e/1FAIpQLSdnlKBKukZhaCntXIsbwAvk0Ot6E7DhjZIJVB4GxcxAo6_m2Q/viewform?usp=header",
+    es: "https://docs.google.com/forms/d/e/1FAIpQLSdnlKBKukZhaCntXIsbwAvk0Ot6E7DhjZIJVB4GxcxAo6_m2Q/viewform?usp=header",
+    de: "https://docs.google.com/forms/d/e/1FAIpQLScqel__49sAKmpt7mPuSAasaBdwosgI1JXnU-eDvEjj-QDMcw/viewform?usp=header"
+  }
+  // Add more lessons here
+};
