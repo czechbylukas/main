c95d647f01ebd38d449770ff4e5106c6f789c8a8
czechbylukas <ucitellukas@gmail.com>
2025-11-02 05:09:36 +0100

    add

--- data/translations/tests_links.js
+++ data/translations/tests_links.js
@@ -2,8 +2,8 @@ const testLinks = {
   lesson1: {
     en: "https://docs.google.com/forms/d/e/1FAIpQLSewHnJaFJZZT2loQHe-JY3g5Y2tCbYecSM8u3TmY6IVEpPOqg/viewform?embedded=true",
     cz: "https://docs.google.com/forms/d/e/1FAIpQLSewHnJaFJZZT2loQHe-JY3g5Y2tCbYecSM8u3TmY6IVEpPOqg/viewform?embedded=true",
-    es: "https://docs.google.com/forms/d/e/.../viewform?embedded=true",
-    de: "https://docs.google.com/forms/d/e/.../viewform?embedded=true"
+    es: "https://docs.google.com/forms/d/e/1FAIpQLSeLwvig7lLI9qebYTr86d4UWhX4w2NOJC0UEWRONH6LZeLXUA/viewform?embedded=true",
+    de: "https://docs.google.com/forms/d/e/1FAIpQLSc-ZrNV1mSxv13fdT_MJ4Ejk6rFNkqieWSvFUPeIF7ogG4Mcw/viewform?embedded=true"
   },
   lesson2: {
     en: "https://docs.google.com/forms/d/e/1FAIpQLSfra5G2LjBzrseHYAZvA7J7f7LxG8NwJgsIvq-fIKXtEZ_gdQ/viewform?embedded=true",
