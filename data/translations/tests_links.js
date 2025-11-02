539c0d62037b163a4468aa8602cf8baef9dbcfb0
czechbylukas <ucitellukas@gmail.com>
2025-11-02 04:44:04 +0100

    embedded google form

--- data/translations/tests_links.js
+++ data/translations/tests_links.js
@@ -1,6 +1,8 @@
 const testLinks = {
   lesson1: {
-    en: "https://docs.google.com/forms/d/e/1FAIpQLSewHnJaFJZZT2loQHe-JY3g5Y2tCbYecSM8u3TmY6IVEpPOqg/viewform?usp=header",
+    en: "<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSewHnJaFJZZT2loQHe-JY3g5Y2tCbYecSM8u3TmY6IVEpPOqg/viewform?embedded=true" width="640" height="1712" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>",
+
+
     cz: "https://docs.google.com/forms/d/e/1FAIpQLSewHnJaFJZZT2loQHe-JY3g5Y2tCbYecSM8u3TmY6IVEpPOqg/viewform?usp=header",
     es: "https://docs.google.com/forms/d/e/1FAIpQLSeLwvig7lLI9qebYTr86d4UWhX4w2NOJC0UEWRONH6LZeLXUA/viewform?usp=header",
     de: "https://docs.google.com/forms/d/e/1FAIpQLSc-ZrNV1mSxv13fdT_MJ4Ejk6rFNkqieWSvFUPeIF7ogG4Mcw/viewform?usp=header"
