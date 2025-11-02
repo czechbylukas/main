447e0957c9fe4fff5f21357c8d200e179bff6ff3
czechbylukas <ucitellukas@gmail.com>
2025-11-02 05:00:56 +0100

    add test link

--- data/translations/tests_links.js
+++ data/translations/tests_links.js
@@ -1,17 +1,14 @@
 const testLinks = {
   lesson1: {
-    en: "<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSewHnJaFJZZT2loQHe-JY3g5Y2tCbYecSM8u3TmY6IVEpPOqg/viewform?embedded=true" width="640" height="1712" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>",
-
-
-    cz: "https://docs.google.com/forms/d/e/1FAIpQLSewHnJaFJZZT2loQHe-JY3g5Y2tCbYecSM8u3TmY6IVEpPOqg/viewform?usp=header",
-    es: "https://docs.google.com/forms/d/e/1FAIpQLSeLwvig7lLI9qebYTr86d4UWhX4w2NOJC0UEWRONH6LZeLXUA/viewform?usp=header",
-    de: "https://docs.google.com/forms/d/e/1FAIpQLSc-ZrNV1mSxv13fdT_MJ4Ejk6rFNkqieWSvFUPeIF7ogG4Mcw/viewform?usp=header"
+    en: "https://docs.google.com/forms/d/e/1FAIpQLSewHnJaFJZZT2loQHe-JY3g5Y2tCbYecSM8u3TmY6IVEpPOqg/viewform?embedded=true",
+    cz: "https://docs.google.com/forms/d/e/1FAIpQLSewHnJaFJZZT2loQHe-JY3g5Y2tCbYecSM8u3TmY6IVEpPOqg/viewform?embedded=true",
+    es: "https://docs.google.com/forms/d/e/.../viewform?embedded=true",
+    de: "https://docs.google.com/forms/d/e/.../viewform?embedded=true"
   },
   lesson2: {
-    en: "https://docs.google.com/forms/d/e/1FAIpQLSdnlKBKukZhaCntXIsbwAvk0Ot6E7DhjZIJVB4GxcxAo6_m2Q/viewform?usp=header",
-    cz: "https://docs.google.com/forms/d/e/1FAIpQLSdnlKBKukZhaCntXIsbwAvk0Ot6E7DhjZIJVB4GxcxAo6_m2Q/viewform?usp=header",
-    es: "https://docs.google.com/forms/d/e/1FAIpQLSdnlKBKukZhaCntXIsbwAvk0Ot6E7DhjZIJVB4GxcxAo6_m2Q/viewform?usp=header",
-    de: "https://docs.google.com/forms/d/e/1FAIpQLScqel__49sAKmpt7mPuSAasaBdwosgI1JXnU-eDvEjj-QDMcw/viewform?usp=header"
+    en: "https://docs.google.com/forms/d/e/1FAIpQLSfra5G2LjBzrseHYAZvA7J7f7LxG8NwJgsIvq-fIKXtEZ_gdQ/viewform?embedded=true",
+    cz: "https://docs.google.com/forms/d/e/1FAIpQLSfra5G2LjBzrseHYAZvA7J7f7LxG8NwJgsIvq-fIKXtEZ_gdQ/viewform?embedded=true",
+    es: "https://docs.google.com/forms/d/e/.../viewform?embedded=true",
+    de: "https://docs.google.com/forms/d/e/.../viewform?embedded=true"
   }
-  // Add more lessons here
 };
