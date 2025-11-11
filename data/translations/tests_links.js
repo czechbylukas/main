e0ca652b2ffd054493c369db8b015148747af18d
czechbylukas <ucitellukas@gmail.com>
2025-11-11 15:51:40 +0000

    add

--- data/translations/tests_links.js
+++ data/translations/tests_links.js
@@ -1,7 +1,7 @@
 const testLinks = {
   lessonUsefulPhrases: {
     en: "https://docs.google.com/forms/d/e/1FAIpQLSewHnJaFJZZT2loQHe-JY3g5Y2tCbYecSM8u3TmY6IVEpPOqg/viewform?embedded=true",
-    es: "https://docs.google.com/forms/d/e/1FAIpQLSeLwvig7lLI9qebYTr86d4UWhX4w2NOJC0UEWRONH6LZeLXUA/viewform?embedded=true,
+    es: "https://docs.google.com/forms/d/e/1FAIpQLSeLwvig7lLI9qebYTr86d4UWhX4w2NOJC0UEWRONH6LZeLXUA/viewform?embedded=true",
     de: "https://docs.google.com/forms/d/e/1FAIpQLSc-ZrNV1mSxv13fdT_MJ4Ejk6rFNkqieWSvFUPeIF7ogG4Mcw/viewform?embedded=true"
   },
   lessonPastTense: {
